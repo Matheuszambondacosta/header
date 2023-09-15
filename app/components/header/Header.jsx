52e65ee7b96a8556d8e95f3f24ae390d025961f5
matheuscosta <matheuszambon564@gmail.com>
2023-09-15 14:33:30 -0300

    feat: put the logo of group

--- app/components/header/Header.jsx
+++ app/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import styles from './Header.module.css';
 import NavLink from '../navlink/NavLink';
-
+import Image from 'next/image';
 export const Header = () => {
     return (
+
         <div className={styles.header}>
-            <p>Octopus</p>
+            <Image className={styles.img} src={'/logo.png'} alt={'Octopus'} width={65} height={65} />   
             <div className={styles.links}>
                 <NavLink rota={'/'} texto={'Index'} />
                 <NavLink rota={'/'} texto={'Sobre Nós'} />
